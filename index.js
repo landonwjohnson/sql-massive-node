7a2dcaae5b7935f102dc1c086db2f855c64ad5ca
Landon <landonwjohnson@gmail.com>
2017-10-08 12:10:17 -0600

    finished

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const massive = require('massive');
 require('dotenv').config()
 const products_controller = require('./products_controller');
 
-
 const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
@@ -16,7 +15,6 @@ app.get( '/api/products', products_controller.getAll );
 app.get( '/api/product/:id', products_controller.getOne );
 app.put( '/api/product/:id', products_controller.update );
 app.delete( '/api/product/:id', products_controller.delete );
-
 
 const port = process.env.PORT || 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
